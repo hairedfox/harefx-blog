cf67b767a6801422bd5b1645b36ca7e1bd67aa1a
Harefx <hai.redfox@outlook.com>
2020-01-12 02:18:16 +0700

    set up rubocop

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -14,8 +14,9 @@ class GraphqlController < ApplicationController
     }
     result = HarefxBlogSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
-  rescue => e
+  rescue StandardError => e
     raise e unless Rails.env.development?
+
     handle_error_in_development e
   end
 
@@ -25,11 +26,9 @@ class GraphqlController < ApplicationController
   def ensure_hash(ambiguous_param)
     case ambiguous_param
     when String
-      if ambiguous_param.present?
-        ensure_hash(JSON.parse(ambiguous_param))
-      else
-        {}
-      end
+      return {} unless ambiguous_param.present?
+
+      ensure_hash(JSON.parse(ambiguous_param))
     when Hash, ActionController::Parameters
       ambiguous_param
     when nil
@@ -39,10 +38,15 @@ class GraphqlController < ApplicationController
     end
   end
 
-  def handle_error_in_development(e)
-    logger.error e.message
-    logger.error e.backtrace.join("\n")
+  def handle_error_in_development(error)
+    logger.error error.message
+    logger.error error.backtrace.join("\n")
 
-    render json: { error: { message: e.message, backtrace: e.backtrace }, data: {} }, status: 500
+    render json: {
+      error: {
+        message: error.message,
+        backtrace: error.backtrace
+      }, data: {}
+    }, status: 500
   end
 end
