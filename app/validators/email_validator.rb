e2b6948409e328b5712585177b04b0cc7d93292a
Harefx <hai.redfox@outlook.com>
2020-01-26 18:35:01 +0700

    move regex into setting file

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,10 +1,8 @@
 class EmailValidator < ActiveModel::EachValidator
-  EMAIL_REGEX = /\A[a-z][a-z0-9.-_]*@[a-z]{2,10}\.[a-z]{2,10}(.[a-z]{2,10})?\Z/.freeze
-
   def validate_each(record, attribute, value)
     message = "must be valid, for examle: valid_email@example.com"
 
-    unless EMAIL_REGEX.match(value)
+    unless Settings.email_pattern.match(value)
       record.errors[attribute] << message
     end
   end
